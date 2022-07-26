ad95caa13c7da0477e92ce90f36ff1e53b1806c4
Mao <mauricio.vera.r7@gmail.com>
2022-07-25 20:03:11 -0400

    simpson_UTF-8 acentos

--- c1/simpson/simpson.js
+++ c1/simpson/simpson.js
@@ -4,13 +4,13 @@ const fs = require('fs').promises
 const hostname = '127.0.0.1'
 const port = 3000 // *
 
-const server = http.createServer(async function (req, res)  { // *
+const server = http.createServer(async (req, res) => { // *
     const contenido = await fs.readFile('frases.txt', 'utf8')
     const myArray = contenido.split("\r\n")
     let frase = myArray[Math.floor(Math.random() * myArray.length)]
 
     res.statusCode = 200 // todo ok
-    res.setHeader('Content-Type', 'text/html')
+    res.setHeader('Content-Type', 'text/html;charset=UTF-8')
     res.end(`<h1>${frase}</h1>`) // *
 })
 
